66fb648b4248c06ca31682e4c609c0e2c3b7d665
AbdullahMuchsin <abdullahmuchsin96@gmail.com>
2024-12-30 23:03:20 +0700

    Update Kode

--- tests/ProductServiceTest.php
+++ tests/ProductServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace AbdullahMuchsin\Test;
 
+use Exception;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
@@ -60,5 +61,39 @@ class ProductServiceTest extends TestCase
         Assert::assertEquals("2", $this->productRepository->findById("2")->getId());
         Assert::assertEquals("3", $this->productRepository->findById("3")->getId());
         Assert::assertEquals("4", $this->productRepository->findById("4")->getId());
+    }
+
+    public function testRegisterSuccess()
+    {
+        $product = new Product();
+        $product->setId("1");
+        $product->setName("Budi");
+
+        $this->productRepository->method("findById")->willReturn(null);
+        $this->productRepository->method("save")->willReturnArgument(0);
+
+        $result = $this->productService->register($product);
+
+        Assert::assertEquals($product->getId(), $result->getId());
+        Assert::assertEquals($product->getName(), $result->getName());
+    }
+
+    public function testRegisterException()
+    {
+        $this->expectException(Exception::class);
+
+        $productInDB = new Product();
+        $productInDB->setId("1");
+        $productInDB->setName("Andi");
+
+        $this->productRepository->method("findById")->willReturn($productInDB);
+        $this->productRepository->method("save")->willReturnArgument(0);
+
+        $product = new Product();
+        $product->setId("1");
+        $product->setId("Andi");
+
+        $this->productService->register($product);
+
     }
 }
